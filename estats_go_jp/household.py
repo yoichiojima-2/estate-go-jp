db4d3135908b1b4ede93cf23c4cd74ef058736b0
Yoichi Ojima <yoichiojima@gmail.com>
2024-06-03 23:45:58 +0900

    misc change

--- estats_go_jp/household.py
+++ estats_go_jp/household.py
@@ -47,6 +47,6 @@ class Household:
             df = (
                 df.merge(self.get_class(cls["@id"]), on=cls["@id"])
                 .drop(columns=[cls["@id"]])
-                .rename(columns={"$": "値", "unit": "単位"})
             )
+        df = df.rename(columns={"$": "値", "unit": "単位"})
         return df[[*[c["@name"] for c in self.classes], "単位", "値"]]
